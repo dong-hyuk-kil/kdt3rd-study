851cc4692dfa672944beb89f705979c9ebc7ff1c
dong-hyuk-kil <ait1029@naver.com>
2022-11-04 12:14:26 +0900

    추가함

--- 12_form_practice/views/index2.js
+++ 12_form_practice/views/index2.js
@@ -4,9 +4,8 @@
 //  성공이든 실패든 무언가의 "최종 결과"
 //  - fulfilled (resolve): 성공
 //  - rejected (reject): 실패
-​
 // promise 객체를 반환하는 promise1 함수 정의
-/*
+
 function promise1(flag) {
   return new Promise(function (resolve, reject) {
     if (flag === true) {
@@ -22,16 +21,14 @@ function promise1(flag) {
     }
   });
 }
-​
-promise1(true) // true, false, 5>3
+promise1(5<3) // true, false, 5>3
   .then(function (result) {
     console.log(result);
   })
   .catch(function (err) {
     console.log(err);
   });
-*/
-​
+
 // #2 Promise 사용법
 // index.js 에서 "콜백함수"를 이용해 동기처리한 것을
 // "promise"를 이용해 동기처리 해보자!!!
@@ -75,11 +72,9 @@ pickDrink().then(pay);
 // reject 도 사용하려는 경우
 // pickDrink().then(pay).catch(nopay);
 */
-​
 // #3 Promise 체이닝
 // ex. 함수를 이용해 (4 + 3) * 2 - 1 = 13 연산을 해보자!
 // add -> mul -> sub 순으로 연산 필요
-​
 // step1. 콜백함수 사용
 /*
 function add(n1, n2, callback) {
@@ -116,50 +111,48 @@ add(4, 3, function (x) {
   });
 });
 */
-​
 // step2. 프로미스 체이닝 사용
-function add(n1, n2) {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      let result = n1 + n2;
-      resolve(result); // resolve(7)
-    }, 1000);
-  });
-}
-​
-function mul(n) {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      let result = n * 2;
-      resolve(result);
-    }, 700);
-  });
-}
-​
-function sub(n) {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      let result = n - 1;
-      resolve(result);
-      // *에러처리: 의도적으로 에러 일으킴
-      //   reject(new Error('의도적으로 에러를 일으켜봤음!!'));
-    }, 500);
-  });
-}
-​
-add(4, 3) // add(4, 3) -> resolve(7) - then(7)
-  .then(function (result) {
-    console.log('1: ', result); // 7
-    return mul(result); // return mul(7) - resolve(14) -> then(14)
-  })
-  .then(function (result) {
-    console.log('2: ', result); // 14
-    return sub(result); // return sub(14) -> resolve(13) -> then(13)
-  })
-  .then(function (result) {
-    console.log('3: ', result); // 13
-  });
-​
+// function add(n1, n2) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       let result = n1 + n2;
+//       resolve(result); // resolve(7)
+//     }, 1000);
+//   });
+// }
+// ​
+// function mul(n) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       let result = n * 2;
+//       resolve(result);
+//     }, 700);
+//   });
+// }
+// ​
+// function sub(n) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       let result = n - 1;
+//       resolve(result);
+//       // *에러처리: 의도적으로 에러 일으킴
+//       //   reject(new Error('의도적으로 에러를 일으켜봤음!!'));
+//     }, 500);
+//   });
+// }
+// ​
+// add(4, 3) // add(4, 3) -> resolve(7) - then(7)
+//   .then(function (result) {
+//     console.log('1: ', result); // 7
+//     return mul(result); // return mul(7) - resolve(14) -> then(14)
+//   })
+//   .then(function (result) {
+//     console.log('2: ', result); // 14
+//     return sub(result); // return sub(14) -> resolve(13) -> then(13)
+//   })
+//   .then(function (result) {
+//     console.log('3: ', result); // 13
+//   });
 // *에러처리
 // add(4, 3) // add(4, 3) -> resolve(7) - then(7)
 //   .then(function (result) {
